c84a81a28ef94ad2bae46f0b9310a64d640c8c6e
SHY <ekdmbzzang@naver.com>
2022-12-01 05:17:06 +0900

    [FIX] index

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import * as serviceWorker from "./serviceWorker";
 import { ChakraProvider, theme } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const client = new QueryClient();
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
@@ -12,10 +14,12 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
 	<React.StrictMode>
-		<ColorModeScript />
-		<ChakraProvider theme={theme}>
-			<RouterProvider router={router} />
-		</ChakraProvider>
+		<QueryClientProvider client={client}>
+			<ColorModeScript />
+			<ChakraProvider theme={theme}>
+				<RouterProvider router={router} />
+			</ChakraProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
 
