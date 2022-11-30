10b38d0f1fa433f71eaed2d3493c75cbabab89b3
SHY <ekdmbzzang@naver.com>
2022-12-01 05:19:16 +0900

    [FEAT] portfolios

--- src/routes/Portfolios.tsx
+++ src/routes/Portfolios.tsx
@@ -6,9 +6,14 @@ import {
 import { Input } from "@chakra-ui/input";
 import { Heading, Text, VStack } from "@chakra-ui/layout";
 import { Textarea } from "@chakra-ui/textarea";
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate, useParams } from "react-router";
+import { getPortfolio } from "../api";
+import { LoadingPage } from "../components/LoadingPage";
 import StyledButton from "../components/StyledButton";
+import useUserProfile from "../lib/useUserProfile";
+import { Iportfolio } from "../types";
 
 export const PortfolioRoot = () => {
 	useEffect(() => {
@@ -55,24 +60,48 @@ export const PortfolioEdit = () => {
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
+
+	const { portfolioId } = useParams();
+	const { isLoggedIn, userLoading, user } = useUserProfile();
+	const navigate = useNavigate();
+
+	const { isLoading, data } = useQuery<Iportfolio>({
+		queryKey: [`portfolios`, portfolioId, `edit`],
+		queryFn: () => getPortfolio(portfolioId),
+	});
+
+	if (!isLoggedIn) navigate("/signin");
+	else if (isLoading) return <LoadingPage />;
+	else if (!data.is_owner) navigate("/");
 	return (
 		<VStack spacing={3}>
 			<Heading size={"md"}>포트폴리오 수정하기</Heading>
 			<FormControl>
 				<FormLabel>제목</FormLabel>
-				<Input type="text" placeholder={"포트폴리오 제목"} />
+				<Input
+					type="text"
+					placeholder={"포트폴리오 제목"}
+					defaultValue={data.title}
+				/>
 				<FormHelperText>
 					포트폴리오를 대표하는 제목을 설정해주세요.
 				</FormHelperText>
 			</FormControl>
 			<FormControl>
 				<FormLabel>설명</FormLabel>
-				<Textarea placeholder={"포트폴리오 설명"} />
+				<Textarea
+					placeholder={"포트폴리오 설명"}
+					defaultValue={data.short_description}
+				/>
 				<FormHelperText>포트폴리오에 대해서 설명해주세요.</FormHelperText>
 			</FormControl>
 			<FormControl>
 				<FormLabel>URL</FormLabel>
-				<Input type="url" placeholder={"https://example.com"} />
+				<Input
+					type="url"
+					placeholder={"https://example.com"}
+					defaultValue={data.url}
+				/>
 				<FormHelperText>포트폴리오 URL을 첨부해주세요.</FormHelperText>
 			</FormControl>
 			<hr />
