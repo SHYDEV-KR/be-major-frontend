56786e6d2ae23537bc196b40e65f840b961617ad
SHY <ekdmbzzang@naver.com>
2022-12-01 05:14:04 +0900

    [FEAT] Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,57 +1,66 @@
 import { Heading, HStack, VStack, Box, Link } from "@chakra-ui/layout";
-import { Children, ReactNode } from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { ColorModeSwitcher } from "../ColorModeSwitcher";
 import { AiOutlineMenu } from "react-icons/ai";
 import { IconButton } from "@chakra-ui/button";
 import { Image } from "@chakra-ui/image";
+import useUserProfile from "../lib/useUserProfile";
 
 interface HeaderProps {
 	onOpen: () => void;
 }
 
 const Header = ({ onOpen }: HeaderProps) => {
-	return (
-		<HStack
-			position={"fixed"}
-			top={0}
-			left={0}
-			right={0}
-			bgColor={"white"}
-			zIndex={999}
-		>
+	const { userLoading, isLoggedIn, user } = useUserProfile();
+	if (!userLoading) {
+		return (
 			<HStack
-				my={"10px"}
-				mx={6}
-				position={"relative"}
-				justifyContent={"center"}
-				width={"100%"}
+				position={"fixed"}
+				top={0}
+				left={0}
+				right={0}
+				bgColor={"white"}
+				zIndex={999}
 			>
-				<IconButton
-					aria-label="Menu"
-					icon={<AiOutlineMenu />}
-					size={"lg"}
-					variant={"ghost"}
-					onClick={onOpen}
-					position={"absolute"}
-					left={0}
-				/>
-				<Link as={RouterLink} to="/">
-					<Image src={"/be_major_logo.png"} maxH={10}></Image>
-				</Link>
-				<HStack position={"absolute"} right={0}>
-					<Link as={RouterLink} to="/my-profile">
-						<Image
-							src={"/logo.png"}
-							borderRadius={"50%"}
-							width={"28px"}
-							height={"28px"}
-						></Image>
+				<HStack
+					my={"10px"}
+					mx={6}
+					position={"relative"}
+					justifyContent={"center"}
+					width={"100%"}
+				>
+					<IconButton
+						aria-label="Menu"
+						icon={<AiOutlineMenu />}
+						size={"lg"}
+						variant={"ghost"}
+						onClick={onOpen}
+						position={"absolute"}
+						left={0}
+					/>
+					<Link as={RouterLink} to="/">
+						<Image src={"/be_major_logo.png"} maxH={10}></Image>
 					</Link>
+					<HStack position={"absolute"} right={0}>
+						<Link as={RouterLink} to="/my-profile">
+							<Image
+								src={
+									isLoggedIn
+										? user?.avatar !== ""
+											? user?.avatar
+											: "/logo.png"
+										: "/logo.png"
+								}
+								borderRadius={"50%"}
+								width={"28px"}
+								height={"28px"}
+								border={"1px solid #DCDCDC"}
+							/>
+						</Link>
+					</HStack>
 				</HStack>
 			</HStack>
-		</HStack>
-	);
+		);
+	}
 };
 
 export default Header;
