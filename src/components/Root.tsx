65225ea6821360b87f5d2a89eb370d421cf180cb
SHY <ekdmbzzang@naver.com>
2022-12-01 05:17:27 +0900

    [FEAT] root

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,4 +1,12 @@
-import { Stack, Box, Divider, VStack, Link } from "@chakra-ui/layout";
+import {
+	Stack,
+	Box,
+	Divider,
+	VStack,
+	Link,
+	Text,
+	Heading,
+} from "@chakra-ui/layout";
 import { Outlet } from "react-router-dom";
 import { Footer } from "./Footer";
 import Header from "./Header";
@@ -12,9 +20,20 @@ import {
 	DrawerOverlay,
 } from "@chakra-ui/modal";
 import { Link as RouterLink } from "react-router-dom";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import useUserProfile from "../lib/useUserProfile";
+import { Button } from "@chakra-ui/button";
+import { signout } from "../api";
+import { Image } from "@chakra-ui/image";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
 
 export default function Root() {
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { user, userLoading, isLoggedIn } = useUserProfile();
+	const navigate = useNavigate();
+	const queryClient = useQueryClient();
+
 	return (
 		<Stack>
 			<Drawer placement={"left"} onClose={onClose} isOpen={isOpen} size={"xs"}>
@@ -23,14 +42,108 @@ export default function Root() {
 					<DrawerCloseButton p={3} />
 					<DrawerHeader>메뉴</DrawerHeader>
 					<DrawerBody>
-						<VStack>
-							<Link as={RouterLink} to="/signin" onClick={onClose}>
-								<Box width={"100%"}>로그인</Box>
-							</Link>
-							<Link as={RouterLink} to="/signup" onClick={onClose}>
-								<Box width={"100%"}>회원가입</Box>
-							</Link>
-						</VStack>
+						{isLoggedIn ? (
+							<VStack>
+								<Link as={RouterLink} to="/my-profile" onClick={onClose}>
+									<Image
+										src={user?.avatar}
+										borderRadius={"50%"}
+										width={"56px"}
+										height={"56px"}
+										border={"1px solid #DCDCDC"}
+									/>
+									<Box width={"100%"}>내 프로필</Box>
+								</Link>
+								<VStack w={"100%"} pt={6}>
+									<Heading
+										alignSelf={"flex-start"}
+										size={"md"}
+										w={"100%"}
+										py={1}
+										_hover={{ bg: "#DCDCDC" }}
+										onClick={() => {
+											navigate("/my-profile/moims/crew");
+										}}
+									>
+										크루로 참여한 모임
+									</Heading>
+									<Heading
+										alignSelf={"flex-start"}
+										size={"md"}
+										w={"100%"}
+										py={1}
+										_hover={{ bg: "#DCDCDC" }}
+										onClick={() => {
+											onClose();
+											navigate("/my-profile/moims/leader");
+										}}
+									>
+										리더로 지원한 모임
+									</Heading>
+									<Heading
+										alignSelf={"flex-start"}
+										size={"md"}
+										w={"100%"}
+										py={1}
+										_hover={{ bg: "#DCDCDC" }}
+										onClick={() => {
+											onClose();
+											navigate("/my-profile/moims/owner");
+										}}
+									>
+										내가 생성한 모임
+									</Heading>
+								</VStack>
+								<Box py={4} w={"100%"}>
+									<Divider />
+								</Box>
+								<Text
+									as={Button}
+									variant={"link"}
+									onClick={async () => {
+										await signout();
+										queryClient.refetchQueries(["users", "my-profile"]);
+										onClose();
+										navigate("/");
+									}}
+									alignSelf={"flex-start"}
+									w={"100%"}
+									py={1}
+									borderRadius={150}
+								>
+									로그아웃
+								</Text>
+							</VStack>
+						) : (
+							<VStack w={"100%"} pt={6}>
+								<Heading
+									alignSelf={"flex-start"}
+									size={"md"}
+									w={"100%"}
+									py={1}
+									_hover={{ bg: "#DCDCDC" }}
+									onClick={() => {
+										onClose();
+										navigate("/signin");
+									}}
+								>
+									로그인
+								</Heading>
+								<Heading
+									alignSelf={"flex-start"}
+									size={"md"}
+									w={"100%"}
+									py={1}
+									_hover={{ bg: "#DCDCDC" }}
+									onClick={() => {
+										onClose();
+										navigate("/signup");
+									}}
+								>
+									회원가입
+								</Heading>
+							</VStack>
+						)}
 					</DrawerBody>
 				</DrawerContent>
 			</Drawer>
