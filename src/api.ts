5643534c2653e869fb5492d84273144b0f2457d9
SHY <ekdmbzzang@naver.com>
2022-12-01 10:17:31 +0900

    [FEAT] domain register

--- src/api.ts
+++ src/api.ts
@@ -6,7 +6,7 @@ const instance = axios.create({
 	baseURL:
 		process.env.NODE_ENV === "development"
 			? "http://127.0.0.1:8000/api/v1/"
-			: "https://be-major.onrender.com/api/v1/",
+			: "https://backend.bemajor.xyz/api/v1/",
 	withCredentials: true,
 });
 
